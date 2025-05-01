6ec84bf0cbbfb3b1ddcefa4988165bd8a2b7080e
Jaku <jacasdavid@gmail.com>
2025-05-01 20:40:41 -0300

    feat: Se agregó una clase de Excepciones

---
+++ Domain/Excepciones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain
+{
+    public class Excepciones
+    {
+        public class MontoNoValido : Exception
+        {
+            public MontoNoValido() : base("El monto ingresado no es válido para la operación solicitada")
+            {
+            }
+        }
+        public class CuentaNoActiva : Exception
+        {
+            public CuentaNoActiva(string estado) : base($"No se puede operar con la cuenta {estado}")
+            {
+            }
+        }
+        public class SaldoInsuficiente : Exception
+        {
+            public SaldoInsuficiente() : base("La cuenta no cuenta con saldo para la operación solicitada. Fue suspendida.")
+            {
+            }
+        }
+    }
+}
