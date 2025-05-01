2a71ce0292759f0a9cd31c16b5cdea938a440036
Jaku <jacasdavid@gmail.com>
2025-05-01 18:01:15 -0300

    feat: Se instanció una CuentaBancaria

--- Domain/CuentaBancaria.cs
+++ Domain/CuentaBancaria.cs
@@ -5,9 +5,8 @@ public class CuentaBancaria
     public TipoCuenta _tipo { get; private set; }
     public string _numero { get; private set; }
     public decimal _saldo { get; set; }
-    public Estado _estado { get; private set; }
     public decimal _tasaDeInteres { get; set; }
-    public decimal _limiteDeDescubierto { get; set; }
+    public Estado _estado { get; set; }
     public decimal _comision { get; set; }
     public string[] _titulares { get; private set; }
 
@@ -142,6 +141,7 @@ public class CajaAhorro : CuentaBancaria
 }
 public class CuentaCorriente : CuentaBancaria
 {
+    public decimal _limiteDeDescubierto { get; set; }
     public CuentaCorriente(string numero, decimal saldo, string[] titulares) : base(numero, saldo, TipoCuenta.CuentaCorriente, titulares)
     {
     }
@@ -153,5 +153,9 @@ public class CuentaCorriente : CuentaBancaria
     private void Retirar(decimal monto)
     {
         _saldo -= monto;
+        if (_saldo - monto >= -_limiteDeDescubierto)
+            _saldo -= monto;
+        if (_saldo < 0)
+            _estado = Estado.Suspendida;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Dsw2025Ej8
+﻿using Dsw2025Ej8.Domain;
+
+namespace Dsw2025Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            CuentaBancaria cuenta = new("0001", 1232.10m, TipoCuenta.CajaDeAhorro, ["Juan Perez", "Maria Lopez"])
+            {
+                _tasaDeInteres = 0.05m,
+            };
         }
     }
 }
