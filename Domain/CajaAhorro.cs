72a33dacd773b4b1f7a5047425e44386839e2872
Jaku <jacasdavid@gmail.com>
2025-05-02 11:03:23 -0300

    fix: Se limpió el código y se borraron librerias innecesarias.

--- Domain/CajaAhorro.cs
+++ Domain/CajaAhorro.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static Dsw2025Ej8.Domain.Excepciones;
+﻿using static Dsw2025Ej8.Domain.Excepciones;
 
 namespace Dsw2025Ej8.Domain
 {
